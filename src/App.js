3772ec3dbd55dd38f7894089050540d31fa98bbc
Jamie <jamieedanielson@gmail.com>
2020-11-06 16:03:58 -0500

    Cleanup, data shows in console

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import MovieGrid from './components/MovieItem';
+import Header from './components/Header.component';
+import MovieGrid from './components/MovieGrid.component';
+import Search from './components/Search.component';
 import './App.css';
+require('dotenv').config();
 
 const App = () => {
 	const [items, setItems] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [query, setQuery] = useState('');
 
 	useEffect(() => {
 		const fetchItems = async () => {
-			const result = await axios(`http://www.omdbapi.com/?apikey=425a8695&t=deadpool`);
+			setIsLoading(true);
+			const result = await axios(
+				`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&t=${query}`,
+			);
 
 			console.log(result.data);
+
 			setItems(result.data);
+			setIsLoading(false);
 		};
 
 		fetchItems();
-	});
+	}, [query]);
 
 	return (
 		<div className="container">
-			<header className="App-header">
-				<MovieGrid items={items} />
-			</header>
+			<Header />
+			<Search getQuery={(q) => setQuery(q)} />
+			<MovieGrid isLoading={isLoading} items={items} />
 		</div>
 	);
 };
---
+++ src/components/MovieDetail.component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MovieDetail = () => {
+	return <div className="card"></div>;
+};
+
+export default MovieDetail;
